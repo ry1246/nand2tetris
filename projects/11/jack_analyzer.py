0e9faeab4d90434834a3732d4f5f82bcad5b6fd9
Ryoma Hara <toooooom720@gmail.com>
2022-05-16 14:53:28 +0900

    implementation: JackAnalylzer

---
+++ projects/11/jack_analyzer.py
@@ -0,0 +1,34 @@
+from const import *
+from compilation_engine import CompilationEngine
+
+import glob
+import argparse
+import os.path
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str, help='source file or directory')
+
+    args = parser.parse_args()
+    path = args.path
+
+    if path.endswith(".jack"):
+        compile(path)
+    else:
+        if path.endswith("/"):
+            path = path[:-1]
+        files = glob.glob("%s/*" % path)
+        for filepath in files:
+            if filepath.endswith(".jack"):
+                compile(filepath)
+
+def compile(filepath):
+    with VmWriter(filepatg[:-5] + ".vm") as code_writer
+        with CompilationEngine(filepath, code_wrier) as ce:
+            print("compiling %s ..." % filepath)
+            ce.compile()
+
+
+if __name__ == '__main__':
+    main()
